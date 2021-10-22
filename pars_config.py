5268ee385d02c4a49a53798bba6d16e1a2b02f10
GrandMa52 <danila.mironov.1994@mail.ru>
2021-10-22 10:27:34 +0300

    Логи добавлятся в файл, перезапись файла отменена, выставлена кодировка файла UTF-8: encoding='utf-8', filemode='a',

--- pars_config.py
+++ pars_config.py
@@ -5,6 +5,7 @@ class Config:
     """
     конфигураия API парсера
     """
+    # noinspection SpellCheckingInspection
     access_token = '390afbf1390afbf1390afbf1153973c4383390a390afbf158326a7fc9e2a40fff702203'
     ver = '5.131'
     lang = 'ru'
@@ -15,8 +16,11 @@ class Logger:
     """
     Конфигурация логирования
     """
+    # noinspection SpellCheckingInspection,PyArgumentList
+    # for row 22-23 (typo,encoding)
     logging.basicConfig(filename='pars_log.txt',
-                        filemode='w',
+                        encoding='utf-8',
+                        filemode='a',
                         format='%(asctime)s|%(process)d|%(levelname)s|%(message)s',
                         level=logging.INFO)
     log = logging.getLogger('pars_log')
